1ce6d358c67e3a282e69795f87db64b9d9cd7967
wizo06 <14183421+wizo06@users.noreply.github.com>
2022-06-06 22:59:54 -0400

    finished discord

--- internal/discord/index.js
+++ internal/discord/index.js
@@ -1,11 +1,40 @@
 const { Client, Intents } = require("discord.js");
 const { logger } = require(process.cwd() + "/pkg/logger");
-const service = require(process.cwd() + "/internal/service");
+const { handleCommandInteraction } = require(process.cwd() + "/internal/discord/commandInteraction");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on("interactionCreate", async (interaction) => {
-  service.HandleInteraction(interaction);  
+  if (interaction.isAutocomplete()) {
+  }
+
+  if (interaction.isButton()) {
+  }
+
+  if (interaction.isCommand()) {
+    handleCommandInteraction(interaction);
+  }
+
+  if (interaction.isContextMenu()) {
+  }
+
+  if (interaction.isMessageComponent()) {
+  }
+
+  if (interaction.isMessageContextMenu()) {
+  }
+
+  if (interaction.isModalSubmit()) {
+  }
+
+  if (interaction.isRepliable()) {
+  }
+
+  if (interaction.isSelectMenu()) {
+  }
+
+  if (interaction.isUserContextMenu()) {
+  }
 });
 
 client.once("ready", () => {
