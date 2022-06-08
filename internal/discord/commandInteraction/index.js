56ed45b4b63cf69dfcc460300050f7991e3ef272
wizo06 <14183421+wizo06@users.noreply.github.com>
2022-06-07 22:25:12 -0400

    try catch

--- internal/discord/commandInteraction/index.js
+++ internal/discord/commandInteraction/index.js
@@ -1,18 +1,23 @@
-const { handleDiscordCommandInteraction } = require(process.cwd() + "/internal/discord/commandInteraction/discord.js")
-const { handleTwitchCommandInteraction } = require(process.cwd() + "/internal/discord/commandInteraction/twitch.js")
+const { logger } = require(process.cwd() + "/pkg/logger");
+const { handleDiscordCommandInteraction } = require(process.cwd() + "/internal/discord/commandInteraction/discord.js");
+const { handleTwitchCommandInteraction } = require(process.cwd() + "/internal/discord/commandInteraction/twitch.js");
 
 const handleCommandInteraction = async (interaction) => {
-  if (interaction.commandName === "discord") {
-    handleDiscordCommandInteraction(interaction);
-    return;
-  }
+  try {
+    if (interaction.commandName === "discord") {
+      handleDiscordCommandInteraction(interaction);
+      return;
+    }
 
-  if (interaction.commandName === "twitch") {
-    handleTwitchCommandInteraction(interaction);
-    return;
+    if (interaction.commandName === "twitch") {
+      handleTwitchCommandInteraction(interaction);
+      return;
+    }
+  } catch (e) {
+    logger.error(e);
   }
-}
+};
 
 module.exports = {
   handleCommandInteraction,
-}
+};
--- internal/discord/index.js
+++ internal/discord/index.js
@@ -5,35 +5,39 @@ const { handleCommandInteraction } = require(process.cwd() + "/internal/discord/
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on("interactionCreate", async (interaction) => {
-  if (interaction.isAutocomplete()) {
-  }
+  try {
+    if (interaction.isAutocomplete()) {
+    }
 
-  if (interaction.isButton()) {
-  }
+    if (interaction.isButton()) {
+    }
 
-  if (interaction.isCommand()) {
-    handleCommandInteraction(interaction);
-  }
+    if (interaction.isCommand()) {
+      handleCommandInteraction(interaction);
+    }
 
-  if (interaction.isContextMenu()) {
-  }
+    if (interaction.isContextMenu()) {
+    }
 
-  if (interaction.isMessageComponent()) {
-  }
+    if (interaction.isMessageComponent()) {
+    }
 
-  if (interaction.isMessageContextMenu()) {
-  }
+    if (interaction.isMessageContextMenu()) {
+    }
 
-  if (interaction.isModalSubmit()) {
-  }
+    if (interaction.isModalSubmit()) {
+    }
 
-  if (interaction.isRepliable()) {
-  }
+    if (interaction.isRepliable()) {
+    }
 
-  if (interaction.isSelectMenu()) {
-  }
+    if (interaction.isSelectMenu()) {
+    }
 
-  if (interaction.isUserContextMenu()) {
+    if (interaction.isUserContextMenu()) {
+    }
+  } catch (e) {
+    logger.error(e);
   }
 });
 
